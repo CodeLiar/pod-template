be1431db6356e8ab7bb076f6a295f2e4297ef741
Geass <mingjieding@linkdoc.com>
2020-03-13 16:29:20 +0800

    <MODIFY>pod 文件升级

--- setup/ConfigureSwift.rb
+++ setup/ConfigureSwift.rb
@@ -17,8 +17,8 @@ module Pod
       framework = configurator.ask_with_answers("Which testing frameworks will you use", ["Quick", "None"]).to_sym
       case framework
         when :quick
-          configurator.add_pod_to_podfile "Quick', '~> 1.2.0"
-          configurator.add_pod_to_podfile "Nimble', '~> 7.0"
+          configurator.add_pod_to_podfile "Quick"
+          configurator.add_pod_to_podfile "Nimble"
           configurator.set_test_framework "quick", "swift", "swift"
 
         when :none
@@ -28,15 +28,13 @@ module Pod
       snapshots = configurator.ask_with_answers("Would you like to do view based testing", ["Yes", "No"]).to_sym
       case snapshots
         when :yes
-          configurator.add_pod_to_podfile "FBSnapshotTestCase' , '~> 2.1.4"
-
           if keep_demo == :no
               puts " Putting demo application back in, you cannot do view tests without a host application."
               keep_demo = :yes
           end
 
           if framework == :quick
-              configurator.add_pod_to_podfile "Nimble-Snapshots' , '~> 6.3.0"
+              configurator.add_pod_to_podfile "Nimble-Snapshots"
           end
       end
 
